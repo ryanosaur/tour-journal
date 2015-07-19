44c6b887fa5c532bf4827d751a09babc518ca1b0
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-19 03:39:27 -0700

    add all endpoints for artist

--- backend/models/profileSchema.js
+++ backend/models/profileSchema.js
@@ -1,5 +1,7 @@
 var profile = function ProfileSchema(params) {
   this.id = params.username || 'undefined';
+  this.password = params.password;
+  this.email = params.email; 
   this.userType = params.userType || 'undefined';
   this.genre = params.genre || 'undefined';
   this.location = params.location || 'undefined';
