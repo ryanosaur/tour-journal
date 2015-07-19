79185b47399e3b178340157f50b3e02689c27548
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-19 05:01:12 -0700

    add booking schema & endpoints

---
+++ backend/models/bookingSchema.js
@@ -0,0 +1,9 @@
+var booking = function BookingSchema(params) {
+  this.venue = "";
+  this.artist = "";
+  this.time = "";
+  this.date = "";
+  this.logistics = [];
+};
+
+module.exports = booking;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var cps = require('cps-api');
-var ProfileSchema = require('../models/profileSchema.js')
+var BookingSchema = require('../models/bookingSchema.js');
+var ProfileSchema = require('../models/profileSchema.js');
 var cpsConn = new cps.Connection('tcp://cloud-eu-0.clusterpoint.com:9007', 'Database',
             process.env.CLUSTERPOINT_USERNAME, process.env.CLUSTERPOINT_PASSWORD,
             'document', 'document/id', {account: 1393});
@@ -113,5 +114,47 @@ router.delete('/venues/:id', function(req, res, next){
   });
 });
 
+router.get('/events/', function(req, res, next){
+  var search_req = new cps.SearchRequest(cps.Term("event", "userType"),
+      0, 20);
+    cpsConn.sendRequest(search_req, function (err, search_resp) {
+       if (err) return console.log(err);
+       console.log(search_resp.results.document);
+    });
+});
+
+router.post('/events', function(req, res, next) {
+  var newEvent = new BookingSchema(req.body);
+  var document = [newProfile];
+  cpsConn.sendRequest(new cps.InsertRequest(document), function (err, resp) {
+    if (err){
+      res.json(err);
+    }
+     res.json(resp);
+  });
+});
+
+router.patch('/events/:id', function(req, res, next){
+  var replace_request = new cps.PartialReplaceRequest(req.body);
+  cpsConn.sendRequest(replace_request, function (err, replace_resp) {
+     if (err){
+       res.json(err);
+     }
+     if (replace_resp) {
+        res.json(replace_resp);
+     }
+  }, 'json');
+});
+
+router.delete('/events/:id', function(req, res, next){
+  cpsConn.sendRequest(new cps.DeleteRequest({ id: req.params.id }), function (err, delete_resp) {
+     if (err){
+       res.json(err);
+     }
+     if(delete_resp){
+      res.json(delete_resp);
+     }
+  });
+});
 
 module.exports = router;
